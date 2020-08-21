0b5b3ac68420cc431e497b4dab996d4e7c4bb6a4
rominmx <maxim.romin@gmail.com>
2020-08-21 17:13:31 +0500

    Add custom fonts, add stylesheets to ViewTask component

--- src/components/ViewTask.vue
+++ src/components/ViewTask.vue
@@ -1,12 +1,10 @@
 <template>
   <div :class="$style.container">
-    <div :class="$style.content">
-      <div :class="$style.time">{{ displayTime | formatTime }}</div>
-      <h3 :class="$style.title">{{ title }}</h3>
-      <button @click="stop">
-        Stop
-      </button>
-    </div>
+    <div :class="$style.time">{{ displayTime | formatTime }}</div>
+    <h3 :class="$style.title">{{ title }}</h3>
+    <button :class="[$button.container, $button.large, $style.button]" @click="stop">
+      Stop
+    </button>
   </div>
 </template>
 
@@ -59,7 +57,29 @@ export default {
   bottom: 0;
   z-index: 10;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  background-color: #fff;
+  padding-left: 1vw;
+  padding-right: 1vw;
+}
+
+.time {
+  font-size: 10vw;
+  line-height: 1;
+}
+
+.title {
+  font-size: 6vw;
+  line-height: 1;
+  margin-top: 0.5em;
+  text-align: center;
+}
+
+.button {
+  font-size: 5vw;
 }
 </style>
+
+<style lang="scss" src="../stylesheets/buttons.scss" module="$button" />
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,6 @@
 const totalTime = (state) =>
   // eslint-disable-next-line
-   state.tasks.map((task) => task.totalTime).reduce((prev, next) => prev + next, 0);
+  state.tasks.map((task) => task.totalTime).reduce((prev, next) => prev + next, 0);
 export default {
   totalTime,
 };
